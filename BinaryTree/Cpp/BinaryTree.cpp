5112831e31ef7d716a9c19c274e9ebd4b93f1132
berketonoz <btonoz@sabanciuniv.edu>
2024-10-07 07:11:58 +0300

    testcase changed

--- BinaryTree/Cpp/BinaryTree.cpp
+++ BinaryTree/Cpp/BinaryTree.cpp
@@ -3,11 +3,11 @@
 
 int main(){
     BinaryTree<int> *tree = new BinaryTree<int>();
-    tree->insert(13);
     tree->insert(31);
-    tree->insert(72);
+    tree->insert(13);
+    tree->insert(23);
     tree->insert(54);
-    tree->insert(66);
+    tree->insert(72);
     tree->insert(91);
 
     tree->inorder();
@@ -17,11 +17,11 @@ int main(){
     tree->inorder();
     tree->deleteNode(13);
     tree->inorder();
-    tree->deleteNode(72);
-    tree->inorder();
     tree->deleteNode(31);
     tree->inorder();
     tree->deleteNode(54);
+    tree->inorder();
+    tree->deleteNode(13);
     tree->inorder();
     tree->~BinaryTree();
     std::cout << "Tree destroyed" << std::endl;
